0bf95002ba8f34001e7e02bc10bd06052ebf400d
Michael Cheng <michael@michaelcheng.us>
2015-06-17 09:59:55 +0800

    Rewrite to use prototype. Toasts are now queued instead of shown all at once

--- toast.js
+++ toast.js
@@ -15,213 +15,182 @@ var iqwerty = iqwerty || {};
 
 iqwerty.toast = (function() {
 
-	/**
-	 * Toast object
-	 * @param {String} text    The text to display in the toast
-	 * @param {Object} options Options for the toast. The options object has the following structure
-	 *
-	 * var opt = {
-	 * 		style: {
-	 * 			//just css styles in here, like:
-	 * 			"background": "pink"
-	 * 			//...
-	 * 		},
-	 * 		settings: {
-	 * 			duration: 3000
-	 * 		}
-	 * }
-	 *
-	 * The default duration is 3000ms, or 3 seconds. This is how long the toast stays on screen. By default, the toast will show for 3 seconds and then slide away.
-	 */
 	function Toast(text, options) {
-		// define some constant settings
-		var TOAST_ANIMATION_SPEED = 400; // the fade in/out speed
-		// some toast identifiers
-		var CLASS_TOAST_GONE = "iqwerty_toast_gone";
-		var CLASS_TOAST_VISIBLE = "iqwerty_toast_visible";
-		var CLASS_TOAST_ANIMATED = "iqwerty_toast_animated";
+		if(getToastStage() != null) {
+			Toast.prototype.toastQueue.push({text: text, options: options});
+		} else {
+			var options = options == undefined ? {} : options;
+			Toast.prototype.options = Toast.prototype.mergeOptions(Toast.prototype.DEFAULT_SETTINGS, options);
 
 
-		var options = options == undefined ? {} : options;
-
-		/**
-		 * The default settings for the toast, including styles and timing options
-		 * @type {Object}
-		 */
-		var _defaultSettings = {
-			style: {
-				"background": "rgba(0, 0, 0, .85)",
-				"box-shadow": "0 0 10px rgba(0, 0, 0, .8)",
-				"z-index": "99999",
-				"border-radius": "3px",
-				"color": "rgba(255, 255, 255, .9)",
-				"padding": "10px 15px",
-				"max-width": "40%",
-				"word-break": "keep-all",
-				"margin": "0 auto",
-				"text-align": "center",
-				"position": "fixed",
-				"left": "0",
-				"right": "0"
-			},
-			settings: {
-				duration: 4000
-			}
-		};
-
-		/**
-		 * Apply the settings for the toast. If settings are customized, they are merged here.
-		 * @return Returns nothing
-		 */
-		function setOptions() {
-			/**
-			 * Merge the options recursively and set them globally.
-			 * @param  {Object} initial The initial default settings
-			 * @param  {Object} custom  The customized settings object
-			 * @return {Object}         The merged settings object
-			 */
-			options = (function mergeOptions(initial, custom) {
-				var merged = custom;
-				for(var prop in initial) {
-					if(merged.hasOwnProperty(prop)) {
-						if(initial[prop] != null && initial[prop].constructor == Object) {
-							merged[prop] = mergeOptions(initial[prop], merged[prop]);
-						}
-					} else {
-						merged[prop] = initial[prop];
-					}
-				}
-				return merged;
-			})(_defaultSettings, options);
-		};
-
-		/**
-		 * If the stylesheet doesn't exist in the page, create it. The stylesheet contains animation settings for the toast.
-		 * @return  Returns nothing
-		 */
-		function initializeStyles() {
-			if(Toast.prototype.styleExists) return;
-
-			var style = document.createElement("style");
-			style.innerHTML = "." + CLASS_TOAST_GONE +
-			"{opacity: 0; bottom: -10%;}" +
-
-			"." + CLASS_TOAST_VISIBLE +
-			"{opacity: 1; bottom: 10%;}" +
-
-			"." + CLASS_TOAST_ANIMATED +
-			"{transition: opacity " + TOAST_ANIMATION_SPEED + "ms, bottom " + TOAST_ANIMATION_SPEED + "ms;}";
-
-			document.head.appendChild(style);
-			style = null;
-
-			// Notify that the stylesheet exists to avoid creating more
-			Toast.prototype.styleExists = true;
-		};
-
-		/**
-		 * The Toast stage. This is the HTML element of the toast.
-		 * @type {Object}
-		 */
-		var _toastStage = null;
-		function getToastStage() {
-			return _toastStage;
-		};
-		function setToastStage(toastStage) {
-			_toastStage = toastStage;
-		};
-
-		/**
-		 * Generate the toast element and apply the styles from the settings.
-		 * @return  Returns nothing
-		 */
-		function generate() {
-			var toastStage = document.createElement("div");
-			var textStage = document.createTextNode(text);
-
-			toastStage.appendChild(textStage);
-
-			setToastStage(toastStage);
-			toastStage = null;
-			textStage = null;
-
-
-			/**
-			 * Stylize the toast
-			 * @return  Returns nothing
-			 */
-			(function stylize() {
-				var toastStage = getToastStage();
-				var s = Object.keys(options.style);
-				s.forEach(function(value, index, array) {
-					toastStage.style[value] = options.style[value];
-				});
-
-				toastStage = null;
-				s = null;
-			})();
+			Toast.prototype.show(text);
 		}
-
-		/**
-		 * Show the Toast. Classes are modified to allow CSS3 animations
-		 * @return  Returns nothing
-		 */
-		(function show() {
-			setOptions();
-			initializeStyles();
-			generate();
-
-			var toastStage = getToastStage();
-			toastStage.classList.add(CLASS_TOAST_ANIMATED);
-			toastStage.classList.add(CLASS_TOAST_GONE);
-			document.body.insertBefore(toastStage, document.body.firstChild);
-			toastStage.offsetHeight;
-			toastStage.classList.remove(CLASS_TOAST_GONE);
-			toastStage.classList.add(CLASS_TOAST_VISIBLE);
-
-			var toastStage = null;
-
-
-			// Hide the Toast after the specified time
-			setTimeout(hide, options.settings.duration);
-		})();
-
-		/**
-		 * Animate the Toast away. Slide/fade out.
-		 * @return  Returns nothing
-		 */
-		function hide() {
-			var toastStage = getToastStage();
-			toastStage.classList.remove(CLASS_TOAST_VISIBLE);
-			toastStage.classList.add(CLASS_TOAST_GONE);
-			toastStage = null;
-
-			// Destroy the Toast element after animations end
-			setTimeout(destroy, TOAST_ANIMATION_SPEED);
-		};
-
-		/**
-		 * Remove the Toast from the DOM and perform some cleanup
-		 * @return  Returns nothing
-		 */
-		function destroy() {
-			var toastStage = getToastStage();
-			document.body.removeChild(toastStage);
-
-			toastStage = null;
-			setToastStage(null);
-		};
 	};
 
+
+	// define some Toast constants
+	
 	/**
-	 * Specifies whether or not the inline stylesheet for CSS3 animations exists. This is used to avoid creating unnecessary multiple stylesheets in the page
-	 * @type {Boolean}
+	 * The Toast animation speed; how long the Toast takes to move to and from the screen
+	 * @type {Number}
 	 */
+	Toast.prototype.TOAST_ANIMATION_SPEED = 400;
+
+	// Toast classes
+	Toast.prototype.CLASS_TOAST_GONE = "iqwerty_toast_gone";
+	Toast.prototype.CLASS_TOAST_VISIBLE = "iqwerty_toast_visible";
+	Toast.prototype.CLASS_TOAST_ANIMATED = "iqwerty_toast_animated";
+
+	/**
+	 * The default Toast settings
+	 * @type {Object}
+	 */
+	Toast.prototype.DEFAULT_SETTINGS = {
+		style: {
+			"background": "rgba(0, 0, 0, .85)",
+			"box-shadow": "0 0 10px rgba(0, 0, 0, .8)",
+			"z-index": "99999",
+			"border-radius": "3px",
+			"color": "rgba(255, 255, 255, .9)",
+			"padding": "10px 15px",
+			"max-width": "40%",
+			"word-break": "keep-all",
+			"margin": "0 auto",
+			"text-align": "center",
+			"position": "fixed",
+			"left": "0",
+			"right": "0"
+		},
+		settings: {
+			duration: 4000
+		}
+	};
+
 	Toast.prototype.styleExists = false;
 
-	return {
-		Toast: function(text, options) {
-			Toast(text, options);
+	Toast.prototype.options = {};
+
+	Toast.prototype.toastQueue = [];
+
+	/**
+	 * Merge the options
+	 * @param  {Object} options The user defined options
+	 */
+	Toast.prototype.mergeOptions = function(initialOptions, customOptions) {
+		var merged = customOptions;
+		for(var prop in initialOptions) {
+			if(merged.hasOwnProperty(prop)) {
+				if(initialOptions[prop] != null && initialOptions[prop].constructor == Object) {
+					merged[prop] = Toast.prototype.mergeOptions(initialOptions[prop], merged[prop]);
+				}
+			} else {
+				merged[prop] = initialOptions[prop];
+			}
 		}
-	}
+
+
+		return merged;
+	};
+
+	Toast.prototype.initializeStyles = function() {
+		if(Toast.prototype.styleExists) return;
+
+		var style = document.createElement("style");
+		style.innerHTML = "." + this.CLASS_TOAST_GONE +
+		"{opacity: 0; bottom: -10%;}" +
+
+		"." + this.CLASS_TOAST_VISIBLE +
+		"{opacity: 1; bottom: 10%;}" +
+
+		"." + this.CLASS_TOAST_ANIMATED +
+		"{transition: opacity " + this.TOAST_ANIMATION_SPEED + "ms, bottom " + this.TOAST_ANIMATION_SPEED + "ms;}";
+
+		document.head.appendChild(style);
+		style = null;
+
+		// Notify that the stylesheet exists to avoid creating more
+		Toast.prototype.styleExists = true;
+	};
+
+	var _toastStage = null;
+	function setToastStage(toastStage) {
+		_toastStage = toastStage;
+	};
+	function getToastStage() {
+		return _toastStage;
+	};
+
+	Toast.prototype.generate = function(text) {
+		var toastStage = document.createElement("div");
+		var textStage = document.createTextNode(text);
+
+		toastStage.appendChild(textStage);
+
+		setToastStage(toastStage);
+		toastStage = null;
+		textStage = null;
+
+		/**
+		 * Stylize the toast
+		 * @return  Returns nothing
+		 */
+		(function stylize() {
+			var toastStage = getToastStage();
+			var s = Object.keys(Toast.prototype.options.style);
+			s.forEach(function(style) {
+				toastStage.style[style] = Toast.prototype.options.style[style];
+			});
+
+			toastStage = null;
+			s = null;
+		}.bind(this))();
+	};
+
+	Toast.prototype.show = function(text) {
+		this.initializeStyles();
+		this.generate(text);
+		
+		var toastStage = getToastStage();
+		toastStage.classList.add(this.CLASS_TOAST_ANIMATED);
+		toastStage.classList.add(this.CLASS_TOAST_GONE);
+		document.body.insertBefore(toastStage, document.body.firstChild);
+		toastStage.offsetHeight;
+		toastStage.classList.remove(this.CLASS_TOAST_GONE);
+		toastStage.classList.add(this.CLASS_TOAST_VISIBLE);
+
+		var toastStage = null;
+
+
+		// Hide the Toast after the specified time
+		setTimeout(Toast.prototype.hide, Toast.prototype.options.settings.duration);
+	};
+
+	Toast.prototype.hide = function() {
+		var toastStage = getToastStage();
+		toastStage.classList.remove(Toast.prototype.CLASS_TOAST_VISIBLE);
+		toastStage.classList.add(Toast.prototype.CLASS_TOAST_GONE);
+		toastStage = null;
+
+		// Destroy the Toast element after animations end
+		setTimeout(Toast.prototype.destroy, Toast.prototype.TOAST_ANIMATION_SPEED);
+	};
+
+	Toast.prototype.destroy = function() {
+		var toastStage = getToastStage();
+		document.body.removeChild(toastStage);
+
+		toastStage = null;
+		setToastStage(null);
+
+
+		if(Toast.prototype.toastQueue.length != 0) {
+			var toast = Toast.prototype.toastQueue.shift();
+			Toast(toast.text, toast.options);
+			toast = null;
+		}
+	};
+
+	return {
+		Toast: Toast
+	};
 })();
