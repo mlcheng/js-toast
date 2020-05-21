ebd3c889a1abaad615712485ce864d92aab4c7c0
Michael Cheng <michael@michaelcheng.us>
2020-05-21 16:18:53 -0500

    Update to ceres build. Note there might be breaking changes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a small JavaScript library that produces a toast message. Toasts are small, unobtrusive notifictions to alert the user of something. This toast slides in from the bottom of the page, stays for a specified amount of time, and then slides away. This is much friendlier than using the `alert()` function in JavaScript.
 
-A demo is available on my [playground](https://www.michaelcheng.us/playground/lib-js/toast/).
+A demo is available on my [playground](https://playground.michaelcheng.us/lib-js/toast/).
 
 ## Usage
 Usage is extremely simple, but advanced customization can also be done. For starters, a toast can be created when a button is clicked
@@ -15,11 +15,20 @@ Where the `showAToast()` function creates and shows a toast
 
 ```javascript
 function showAToast() {
-	iqwerty.toast.Toast('Hello!');
+	iqwerty.toast.toast('Hello!');
 }
 ```
 
-This shows a toast with default settings
+This shows a toast with default settings.
+
+If you'd like to close any toast, grab a reference to any toast and call `.hide()`.
+
+```js
+const toast = iqwerty.toast.toast('Hello!');
+toast.hide();
+```
+
+Note this closes any toast currently visible, not the one you have a reference to. Because programming is hard :(
 
 ## Advanced customization
 The toast is easy to use, and it looks great by default. However, it can also be customized to your liking.
@@ -28,31 +37,34 @@ The toast is easy to use, and it looks great by default. However, it can also be
 The toast style can be customized by specifying a style in the options parameter
 
 ```javascript
-var options = {
+const options = {
 	style: {
 		main: {
 			background: "pink",
-			color: "black"
-		}
-	}
+			color: "black",
+		},
+	},
 };
 
-iqwerty.toast.Toast('Hello!', options);
+iqwerty.toast.toast('Hello!', options);
 ```
 
-The styles are in CSS-ish syntax
+The styles are in CSS-ish syntax.
 
 ### Duration
-The duration of the toast (the time that the toast stays on the screen) is set to 4000ms (4s) by default, but you can change this if you wish
+The duration of the toast (the time that the toast stays on the screen) is set to 4000ms (4s) by default, but you can change this if you wish. If set to `0`, the toast will remain visible forever until `.hide()` is called.
 
 ```javascript
-var options = {
+const options = {
 	settings: {
-		duration: 5000
-	}
+		duration: 5000,
+	},
 };
 
-iqwerty.toast.Toast('Hello!', options);
+iqwerty.toast.toast('Hello!', options);
 ```
 
 Have fun!
+
+## Accessibility (a11y)
+I will add `aria-live` for a11y when I have time... Sorry to users with disabilities that are using this now :(
--- toast.js
+++ toast.js
@@ -11,13 +11,39 @@
 
 'use strict';
 
-var iqwerty = iqwerty || {};
+/**
+ * Merge the DEFAULT_SETTINGS with the user defined options if specified
+ * @param {Object} options The user defined options
+ */
+export function mergeOptions(initialOptions, customOptions) {
+	const merged = customOptions;
+	for(const prop in initialOptions) {
+		if(merged.hasOwnProperty(prop)) {
+			if(initialOptions[prop] !== null && initialOptions[prop].constructor === Object) {
+				merged[prop] = mergeOptions(initialOptions[prop], merged[prop]);
+			}
+		} else {
+			merged[prop] = initialOptions[prop];
+		}
+	}
+	return merged;
+}
 
-iqwerty.toast = (function() {
+/**
+ * Stylize the Toast.
+ * @param {Element} element The HTML element to stylize
+ * @param {Object}  styles  An object containing the style to apply
+ */
+export function stylize(element, styles) {
+	Object.keys(styles).forEach((style) => {
+		element.style[style] = styles[style];
+	});
+}
 
+export const toast = (() => {
 	/**
 	 * The Toast animation speed; how long the Toast takes to move to and from the screen
-	 * @type {Number}
+	 * @type {number}
 	 */
 	const TOAST_ANIMATION_SPEED = 400;
 
@@ -38,54 +64,10 @@ iqwerty.toast = (function() {
 	};
 
 	/**
-	 * The main Toast object
-	 * @param {String} text    The text to put inside the Toast
-	 * @param {Object} options Optional; the Toast options. See Toast.prototype.DEFAULT_SETTINGS for more information
-	 * @param {Object} transitions Optional; the Transitions object. This should not be used unless you know what you're doing
-	 */
-	function Toast(text, options, transitions) {
-		if(getToastStage() !== null) {
-			// If there is already a Toast being shown, put this Toast in the queue to show later
-			Toast.prototype.toastQueue.push({
-				text: text,
-				options: options,
-				transitions: transitions
-			});
-		} else {
-			Toast.prototype.Transitions = transitions || Transitions;
-			var _options = options || {};
-			_options = Toast.prototype.mergeOptions(Toast.prototype.DEFAULT_SETTINGS, _options);
-
-			Toast.prototype.show(text, _options);
-			
-			_options = null;
-		}
-	}
-
-
-	/**
-	 * The toastStage. This is the HTML element in which the toast resides
-	 * Getter and setter methods are available privately
-	 * @type {Element}
-	 */
-	var _toastStage = null;
-	function getToastStage() {
-		return _toastStage;
-	}
-	function setToastStage(toastStage) {
-		_toastStage = toastStage;
-	}
-
-
-
-
-	// define some Toast constants
-
-	/**
 	 * The default Toast settings
 	 * @type {Object}
 	 */
-	Toast.prototype.DEFAULT_SETTINGS = {
+	const DEFAULT_SETTINGS = {
 		style: {
 			main: {
 				'background': 'rgba(0, 0, 0, .85)',
@@ -96,7 +78,8 @@ iqwerty.toast = (function() {
 				'z-index': '99999',
 
 				'color': 'rgba(255, 255, 255, .9)',
-				
+				'font-family': 'sans-serif',
+
 				'padding': '10px 15px',
 				'max-width': '60%',
 				'width': '100%',
@@ -113,164 +96,136 @@ iqwerty.toast = (function() {
 				'transform': 'translateY(150%) translateZ(0)',
 				'-webkit-filter': 'blur(0)',
 				'opacity': '0'
-			}
+			},
 		},
 
 		settings: {
-			duration: 4000
-		}
+			duration: 4000,
+		},
 	};
-
-	Toast.prototype.Transitions = {};
-
 
 	/**
 	 * The queue of Toasts waiting to be shown
 	 * @type {Array}
 	 */
-	Toast.prototype.toastQueue = [];
+	const toastQueue = [];
 
+	/**
+	 * The toastStage. This is the HTML element in which the toast resides
+	 * Getter and setter methods are available privately
+	 * @type {HTMLElement}
+	 */
+	let toastStage;
 
 	/**
 	 * The Timeout object for animations.
 	 * This should be shared among the Toasts, because timeouts may be cancelled e.g. on explicit call of hide()
 	 * @type {Object}
 	 */
-	Toast.prototype.timeout = null;
-
+	let timeout;
 
 	/**
-	 * Merge the DEFAULT_SETTINGS with the user defined options if specified
-	 * @param  {Object} options The user defined options
+	 * The main Toast object
+	 * @param {string} text The text to put inside the Toast
+	 * @param {Object} options Optional; the Toast options. See Toast.prototype.DEFAULT_SETTINGS for more information
+	 * @param {Object} transitions Optional; the Transitions object. This should not be used unless you know what you're doing
+	 * @param {Object} __internalDefaultSettings For internal use only. Used for Snackbar.
 	 */
-	Toast.prototype.mergeOptions = function(initialOptions, customOptions) {
-		var merged = customOptions;
-		for(var prop in initialOptions) {
-			if(merged.hasOwnProperty(prop)) {
-				if(initialOptions[prop] !== null && initialOptions[prop].constructor === Object) {
-					merged[prop] = Toast.prototype.mergeOptions(initialOptions[prop], merged[prop]);
-				}
-			} else {
-				merged[prop] = initialOptions[prop];
-			}
-		}
-		return merged;
-	};
+	function toast(text, options, transitions) {
+		const _transitions = transitions || Transitions;
 
+		if(getToastStage() !== undefined) {
+			// If there is already a Toast being shown, put this Toast in the queue to show later
+			toastQueue.push({ text, options, transitions: _transitions });
+		} else {
+			let _options = options || {};
+			_options = mergeOptions(DEFAULT_SETTINGS, _options);
+
+			showToast(text, _options, _transitions);
+		}
+
+		return {
+			hide: () => hideToast(_transitions),
+		};
+	}
+
+	/**
+	 * Show the Toast
+	 * @param {string} text The text to show inside the Toast
+	 * @param {Object} options The object containing the options for the Toast
+	 */
+	function showToast(text, options, transitions) {
+		generateToast(text, options.style.main);
+
+		const toastStage = getToastStage();
+		document.body.insertBefore(toastStage, document.body.firstChild);
+
+		// This is a hack to get animations started. Apparently without explicitly redrawing, it'll just attach the class and no animations would be done.
+		toastStage.offsetHeight;
+
+		stylize(toastStage, transitions.SHOW);
+
+		// Hide the Toast after the specified time
+		clearTimeout(timeout);
+		if(options.settings.duration !== 0) {
+			timeout = setTimeout(() => hideToast(transitions), options.settings.duration);
+		}
+	}
+
+	/**
+	 * Hide the Toast that's currently shown.
+	 */
+	function hideToast(transitions) {
+		const toastStage = getToastStage();
+		stylize(toastStage, transitions.HIDE);
+
+		// Destroy the Toast element after animations end.
+		clearTimeout(timeout);
+		toastStage.addEventListener('transitionend', destroyToast, { once: true });
+	}
 
 	/**
 	 * Generate the Toast with the specified text.
-	 * @param  {String|Object} text    The text to show inside the Toast, can be an HTML element or plain text
-	 * @param  {Object} style   The style to set for the Toast
+	 * @param {string|HTMLElement} text The text to show inside the Toast, can be an HTML element or plain text
+	 * @param {Object} style The style to set for the Toast
 	 */
-	Toast.prototype.generate = function(text, style) {
+	function generateToast(text, style) {
 		var toastStage = document.createElement('div');
 
-
-		/**
-		 * If the text is a String, create a textNode for appending
-		 */
+		// If the text is a String, create a textNode for appending.
 		if(typeof text === 'string') {
 			text = document.createTextNode(text);
 		}
 		toastStage.appendChild(text);
 
-
 		setToastStage(toastStage);
-		toastStage = null;
-
-		Toast.prototype.stylize(getToastStage(), style);
-	};
+		stylize(getToastStage(), style);
+	}
 
 	/**
-	 * Stylize the Toast.
-	 * @param  {Element} element The HTML element to stylize
-	 * @param  {Object}  styles  An object containing the style to apply
-	 * @return                   Returns nothing
+	 * Clean up after the Toast slides away. Namely, removing the Toast from the DOM.
+	 * After the Toast is cleaned up, display the next Toast in the queue if any exists.
 	 */
-	Toast.prototype.stylize = function(element, styles) {
-		Object.keys(styles).forEach(function(style) {
-			element.style[style] = styles[style];
-		});
-	};
+	function destroyToast() {
+		const toastStage = getToastStage();
 
-
-	/**
-	 * Show the Toast
-	 * @param  {String} text    The text to show inside the Toast
-	 * @param  {Object} options The object containing the options for the Toast
-	 */
-	Toast.prototype.show = function(text, options) {
-		this.generate(text, options.style.main);
-		
-		var toastStage = getToastStage();
-		document.body.insertBefore(toastStage, document.body.firstChild);
-
-
-
-		// This is a hack to get animations started. Apparently without explicitly redrawing, it'll just attach the class and no animations would be done
-		toastStage.offsetHeight;
-
-
-		Toast.prototype.stylize(toastStage, Toast.prototype.Transitions.SHOW);
-
-
-		toastStage = null;
-
-
-		// Hide the Toast after the specified time
-		clearTimeout(Toast.prototype.timeout);
-		Toast.prototype.timeout = setTimeout(Toast.prototype.hide, options.settings.duration);
-	};
-
-
-	/**
-	 * Hide the Toast that's currently shown
-	 */
-	Toast.prototype.hide = function() {
-		var toastStage = getToastStage();
-		Toast.prototype.stylize(toastStage, Toast.prototype.Transitions.HIDE);
-
-		// Destroy the Toast element after animations end
-		clearTimeout(Toast.prototype.timeout);
-		toastStage.addEventListener('transitionend', Toast.prototype.animationListener);
-		toastStage = null;
-	};
-
-	Toast.prototype.animationListener = function() {
-		getToastStage().removeEventListener('transitionend', Toast.prototype.animationListener);
-		Toast.prototype.destroy.call(this);
-	};
-
-
-	/**
-	 * Clean up after the Toast slides away. Namely, removing the Toast from the DOM. After the Toast is cleaned up, display the next Toast in the queue if any exists
-	 */
-	Toast.prototype.destroy = function() {
-		var toastStage = getToastStage();
 		document.body.removeChild(toastStage);
+		setToastStage(undefined);
 
-		toastStage = null;
-		setToastStage(null);
-
-
-		if(Toast.prototype.toastQueue.length > 0) {
-			// Show the rest of the Toasts in the queue if they exist
-			
-			var toast = Toast.prototype.toastQueue.shift();
-			Toast(toast.text, toast.options, toast.transitions);
-
-			// clean up
-			toast = null;
+		if(toastQueue.length > 0) {
+			// Show the rest of the Toasts in the queue if they exist.
+			const newToast = toastQueue.shift();
+			toast(newToast.text, newToast.options, newToast.transitions);
 		}
-	};
+	}
 
-	return {
-		Toast: Toast
-	};
+	function getToastStage() {
+		return toastStage;
+	}
+
+	function setToastStage(newToastStage) {
+		toastStage = newToastStage;
+	}
+
+	return { toast };
 })();
-
-if(typeof module !== 'undefined') {
-	/* global module */
-	module.exports = iqwerty.toast;
-}
